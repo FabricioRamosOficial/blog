a707046f106117999d516854f700d18f1ad35d92
FABRICIO RAMOS COELHO <81585341+FabricioRamosOficial@users.noreply.github.com>
2023-01-04 23:30:40 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'menu',component:MenuComponent},
+  {path:'contato',component:ContatoComponent},
+  {path:'sobre',component:SobreComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
